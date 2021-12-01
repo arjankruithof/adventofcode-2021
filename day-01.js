2f2f51e760d276d7431dcac4ac18899e7efade6c
Arjan Kruithof <xarjanx@gmail.com>
2021-12-01 09:21:06 +0100

    Added day-01

---
+++ day-01.js
@@ -0,0 +1,62 @@
+// Get starttime
+const startTime = new Date().getTime();
+const dayNumber = '01';
+
+// load the file
+const fs = require('fs');
+const path = require('path');
+const appData = fs.readFileSync(path.resolve(__dirname, `day-${dayNumber}.txt`), 'utf8').split('\n');
+console.log('appData:', appData);
+
+// part 1
+let numberOfIncreasedValues = 0;
+let numberOfDescreadedValues = 0;
+let previousValue = 0;
+
+for (let i = 0; i < appData.length; i += 1) {
+    const currentValue = parseInt(appData[i], 10);
+
+    if (previousValue === 0) {
+        previousValue = currentValue;
+    }
+
+    if (currentValue > previousValue) {
+        numberOfIncreasedValues += 1;
+    } else if (currentValue < previousValue) {
+        numberOfDescreadedValues += 1;
+    }
+
+    previousValue = appData[i];
+}
+
+// solution part 1:
+console.log('Solution part 1: number of increased values', numberOfIncreasedValues);
+
+// part 2
+numberOfIncreasedValues = 0;
+numberOfDescreadedValues = 0;
+previousValue = 0;
+
+for (let i = 0; i < appData.length - 2; i += 1) {
+    const sumOfMeasurementWindow = parseInt(appData[i], 10) + parseInt(appData[i + 1], 10) + parseInt(appData[i + 2], 10);
+
+    if (previousValue === 0) {
+        previousValue = sumOfMeasurementWindow;
+    }
+
+    if (sumOfMeasurementWindow > previousValue) {
+        numberOfIncreasedValues += 1;
+    } else if (sumOfMeasurementWindow < previousValue) {
+        numberOfDescreadedValues += 1;
+    }
+
+    previousValue = sumOfMeasurementWindow;
+}
+
+console.log('Solution part 2: number of increased values', numberOfIncreasedValues);
+
+// Get endtime
+const endTime = new Date().getTime();
+
+// Get execution time
+console.log('Execution time:', endTime - startTime, 'ms');
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Advent of Code 2021
 
+Run app with `node day-01.js` in your terminal.
+
 Start every day with a copy of `boilerplate.js`.
